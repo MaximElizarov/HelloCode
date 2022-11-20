ac0c3e2a81933f801bf1c4fa042158ebe3e96ca7
Maxim Elizmarov <kiro.dota322@gmail.com>
2022-11-20 14:14:08 +0300

    add example_005 IF-els

---
+++ Examples_005_ifElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя ползователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "оля")
+{
+    Console.WriteLine("Ура, это же Оля!!!");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
